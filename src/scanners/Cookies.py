1b192c7b38dbf30ff539c280fccd2d9028bba2d0
MouadZIANI <mouad.ziani1997@gmail.com>
2021-05-03 13:02:25 +0000

    feat: wip on insecure headers scanner

--- src/scanners/Cookies.py
+++ src/scanners/Cookies.py
@@ -17,23 +17,3 @@ class Cookies:
 
     def _get_headers(self, page):
         return str(_page.headers).replace("'", "")
-
-
-    def _has_xframe_defence(self):
-        try:
-        	xframe = self._get_headers()['x-frame-options']
-        	print 'X-FRAME-OPTIONS:', xframe , 'present'
-        	return True
-        except:
-        	print 'X-FRAME-OPTIONS missing'
-        	return False
-
-
-    def _has_hsts_defence(self):
-        try:
-        	hsts = self._get_headers()['Strict-Transport-Security']
-        	print 'Strict-Transport-Security:', hsts , 'present'
-        	return True
-        except:
-        	print 'HSTS header not set'
-        	return False
