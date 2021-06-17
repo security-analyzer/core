e4b1146f1c4a4c7cfc5edd7fde63699004b8f333
MouadZIANI <mouad.ziani1997@gmail.com>
2021-06-17 02:03:57 +0100

    feat: code refactoring

--- src/scanners/vulnerabilities/Scanner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class Scanner:
-
-    def __init__(self, domain, headers, contents):
-        self._domain = domain
-        self._contents = contents
-        self._headers = headers
